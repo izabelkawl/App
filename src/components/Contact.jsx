74232572e477c32a5262e7ecad6dbb642a77a5ad
Iza <izabelawlazlo9@gmail.com>
2021-05-14 23:35:25 +0200

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,101 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
-const Contento = styled.div`
-  margin: 0 auto;
-  width: fit-content;
-  text-align: center;
-  padding-top: 200px;
+import bg from "../img/yellow_bg.svg";
+import me from "../img/me.svg";
+import linkedin_icon from "../img/icon_in.svg";
+import facebook_icon from "../img/icon_fb.svg";
+import github_icon from "../img/icon_gh.svg";
+
+const Content = styled.div`
+  box-sizing: content-box;
+  min-height: 70vh !important;
+  display: table;
+  @media (min-width: 1920px) {
+    min-height: 80vh !important;
+    padding: 0 5vw 0;
+  }
+`;
+const ImageBg = styled.img`
+  position: fixed;
+  z-index: 0;
+  bottom: 0;
+  width: 50vw;
+  right: 0;
+  @media (min-width: 768px) {
+    position: absolute;
+    z-index: 1;
+  }
+  @media (min-width: 1920px) {
+    width: 50vw;
+  }
+`;
+const Person = styled.img`
+  position: fixed;
+  z-index: 1;
+  width: 22vw;
+  filter: drop-shadow(6px 6px 4px rgba(0, 0, 0, 0.1));
+  bottom: 0;
+  left: 65vw;
+  @media (min-width: 768px) {
+    position: absolute;
+    z-index: 2;
+  }
+  @media (min-width: 1920px) {
+    width: 25vw;
+  }
+`;
+const IconBox = styled.div`
+  position: fixed;
+  z-index: 2;
+  bottom: 5vh;
+  left: 5vw;
+  @media (min-width: 768px) {
+    position: absolute;
+    z-index: 3;
+    bottom: 10vh;
+  }
+  @media (min-width: 1365px) {
+    left: 6vw;
+  }
+  @media (min-width: 1920px) {
+    left: 10vw;
+  }
 `;
 
+const icon = {
+  paddingRight: "2vw",
+  height: "7vh",
+  filter: "drop-shadow(6px 6px 4px rgba(0, 0, 0, 0.1))",
+};
 class Contact extends Component {
   render() {
     return (
       <>
-        <Contento>
+        <Content>
           <h4>Izabela Wlazło</h4>
           <p>
             <b>+48 733 253 028</b>
           </p>
           <p>izabelawlazlo9@gmail.com</p>
           <p>Rzeszów</p>
-        </Contento>
+
+          <IconBox>
+            <a href="https://www.facebook.com/izabelka279/">
+              <img src={facebook_icon} style={icon} />
+            </a>
+
+            <a href="https://www.linkedin.com/in/izabelawl/">
+              <img src={linkedin_icon} style={icon} />
+            </a>
+            <a href="https://github.com/izabelkawl">
+              <img src={github_icon} style={icon} />
+            </a>
+          </IconBox>
+
+          <ImageBg src={bg} />
+          <Person src={me} />
+        </Content>
       </>
     );
   }
